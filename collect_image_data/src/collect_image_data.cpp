65d2f77871fc2c8c3b0b92171e705cfdfd057a28
Uriel Martinez <uriel.marherz@gmail.com>
2018-01-24 23:21:50 +0000

    added ports to read data from webcam

--- collect_image_data/src/collect_image_data.cpp
+++ collect_image_data/src/collect_image_data.cpp
@@ -1,3 +1,4 @@
+#include "opencv2/core/version.hpp"
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -18,86 +19,129 @@ using namespace cv;
 
 
 int imageCounter = 0;
-bool isThereImage = false;
+bool cameraType = false;
 std::string folderName;
 std::string objectName;
 std::string fileName;
-std::string fileNameProcessed;
 int saveData = 0;
-cv::Mat res;	// added: processing background
+cv::Mat frame;
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
-  try
-  {
-	isThereImage = false;
+	try
+	{
+		cv::Mat hsv_image;	// added: processing background
+		cv::Mat mask;	// added: processing background
+		cv::Mat res;	// added: processing background
 
-	cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::waitKey(30);
+		frame = cv_bridge::toCvShare(msg, "bgr8")->image;
 
-    cv::Mat frame;
+		cv::cvtColor(frame, hsv_image, COLOR_BGR2HSV);	// added: processing background
+		cv::inRange(hsv_image, Scalar(0, 70, 50), Scalar(10, 255, 255), mask);	// added: processing background
+		bitwise_and(frame, frame, res, mask);	// added: processing background
+		frame = res;
+
+		cv::imshow("view", frame);
+		cv::waitKey(30);
+
+		if (saveData == 1)
+		{
+			imageCounter++;
+			fileName.clear();
+			std::stringstream strCounter;
+			strCounter << imageCounter;
+			fileName = folderName + "/" + objectName + "_" + strCounter.str() + ".jpg";
+			cv::imwrite(fileName, frame);
+		}
+	}
+	catch (cv_bridge::Exception& e)
+	{
+		ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+	}
+}
+
+void imageCallback(cv::VideoCapture cap)
+{
+	cv::Mat res;
 	cv::Mat hsv_image;	// added: processing background
 	cv::Mat mask;	// added: processing background
-	frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+
+	cap >> frame;
 
 	cv::cvtColor(frame, hsv_image, COLOR_BGR2HSV);	// added: processing background
 	cv::inRange(hsv_image, Scalar(0, 70, 50), Scalar(10, 255, 255), mask);	// added: processing background
 	bitwise_and(frame, frame, res, mask);	// added: processing background
-	cv::imshow("Processed image", res);	// added: processing background
+	frame = res;
 
-	isThereImage = true;
-
-	if( saveData == 1 )
-	{
-		imageCounter++;
-		fileName.clear();
-		std::stringstream strCounter;
-		strCounter << imageCounter;
-		fileName = folderName + "/" + objectName + "_" + strCounter.str() + ".jpg";
-		fileNameProcessed = folderName + "/" + objectName + "_" + strCounter.str() + "_processed.jpg";
-		cv::imwrite(fileName, frame);
-		cv::imwrite(fileNameProcessed, frame);
-	}
-  }
-  catch (cv_bridge::Exception& e)
-  {
-    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
-  }
+	cv::imshow("view", frame);
+	cv::waitKey(30);
 }
+
+
+void cbfunc()
+{
+	printf("called");
+}
+
+
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "collect_image_data");
-  ros::NodeHandle nh;
+	ros::init(argc, argv, "uav_control");
+	ros::NodeHandle nh;
 
-  std::cout << "Folder name: ";
-  std::cin >> folderName;
-  std::cout << "Object name: ";
-  std::cin >> objectName;
-  std::cout << "Save data [Y=1/N=0]?: ";
-  std:cin >> saveData;
+	std::cout << "Folder name: ";
+	std::cin >> folderName;
+	std::cout << "Object name: ";
+	std::cin >> objectName;
+	std::cout << "Save data [Y=1/N=0]?: ";
+std:cin >> saveData;
+	std::cout << "Webcam [0] or bebop camera [1]?: ";
+	std::cin >> cameraType;
 
-  cv::namedWindow("view");
-  cv::startWindowThread();
-  image_transport::ImageTransport it(nh);
-  image_transport::Subscriber sub = it.subscribe("/bebop/image_raw", 1, imageCallback);
-  
-  image_transport::Publisher pub = it.advertise("/image_processed", 1);	// maybe this needs to be global
-  sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", res).toImageMsg();
+	cv::namedWindow("view");
+	cv::startWindowThread();
 
-  ros::Rate loop_rate(5);
-  
-  while(nh.ok())
-  {
-	  if(isThereImage == true)
-	  {
-		  pub.publish(msg);
-		  ros::spinOnce();
-		  loop_rate.sleep();
-	  }
-  }
+	image_transport::ImageTransport it(nh);
 
-  ros::spin();
+	cv::VideoCapture cap(0); // open the default camera
 
-  cv::destroyWindow("view");
+	if (cameraType == true)
+	{
+		image_transport::Subscriber sub = it.subscribe("/bebop/image_raw", 1, imageCallback);
+
+		ros::spin();
+	}
+	else
+	{
+		if (!cap.isOpened())  // check if we succeeded
+			return -1;
+
+		std::cout << "Webcam opened to read images" << std::endl;
+
+	}
+
+	image_transport::Publisher pub = it.advertise("/image_processed", 1);	// maybe this needs to be global
+	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+
+
+
+
+	ros::Rate loop_rate(100);
+
+	while (nh.ok())
+	{
+		if (cameraType == false)
+		{
+			imageCallback(cap);
+			msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+		}
+
+		pub.publish(msg);
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	cv::destroyWindow("view");
 }
+
